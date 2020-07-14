4509370e4516fd7f32fcd75552226adceda11acf
YuukiAbe <nura1122@icloud.com>
2020-07-14 15:46:22 +0900

    プロジェクト作成

--- BMI/BMI/Program.cs
+++ BMI/BMI/Program.cs
@@ -6,9 +6,36 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ああああ");
 
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < args.Length; i++) 
+            {
+                Console.WriteLine(i + ":" + args[i]);
+            }
+
+            float height = GetInputAndParseFloat("身長を入力してください(m)");
+            float weight = GetInputAndParseFloat("体重を入力してください(kg)");
+            float bmi = CalcBMI(height, weight);
+
+            Console.WriteLine("BMIは" + bmi + "です。");
+        }
+        static float GetInputAndParseFloat(string log)
+        {
+            bool parseSuccess = false;
+            float parsedValue = 0f;
+            while (!parseSuccess)
+            {
+                Console.WriteLine(log);
+                string input = Console.ReadLine();
+                parseSuccess = float.TryParse(input, out parsedValue);
+
+                if (!parseSuccess)
+                    Console.WriteLine("数値を入力して下さい。");
+            }
+            return parsedValue;
+        }
+        static float CalcBMI(float height, float weight)
+        {
+            return weight / (height * height);
         }
     }
 }
